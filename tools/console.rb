35266ca0f5bfe1fbc93a81a14be83efa20eaff9b
sammy-coder <sammymasib000@gmail.com>
2022-10-31 23:10:32 +0300

    Completed assignment

--- tools/console.rb
+++ tools/console.rb
@@ -8,8 +8,14 @@ end
 ###  WRITE YOUR TEST CODE HERE ###
 
 
-
-
+author1 = Author.new("queenslayjema")
+author2 = Author.new("jema")
+forbes = Magazine.new("Forbes","life")
+people = Magazine.new("People","mypeople")
+title = "mytitle"
+puts forbes.contributors
+article1 = Article.new(author1, forbes,"GETTING THAT PAPER")
+article2 = Article.new(author2, forbes,"GETTING  PAPER")
 
 
 
